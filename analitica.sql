d3e3725dfebaa8c7e82f7c747d11d1c3946a4f2f
dylandavid04 <dylan_david82211@elpoli.edu.co>
2024-09-14 17:18:25 -0500

    Add files via upload

---
+++ analitica.sql
@@ -0,0 +1,122 @@
+ /* CREATE TABLE */
+CREATE TABLE IF NOT EXISTS TABLE_NAME(
+País VARCHAR( 100 ),
+CódigoISOpaís VARCHAR( 100 ),
+Oficinaderegistro VARCHAR( 100 ),
+Grupoedad VARCHAR( 100 ),
+Edad(años) INT(11),
+ÁreaConocimiento VARCHAR( 100 ),
+SubAreaConocimiento VARCHAR( 100 ),
+NivelAcadémico VARCHAR( 100 ),
+Estadocivil VARCHAR( 100 ),
+Género VARCHAR( 100 ),
+Etniadelapersona VARCHAR( 100 ),
+Estatura(CM) INT(11),
+Localización VARCHAR( 100 ),
+Cantidaddepersonas INT(11)
+);
+
+/* INSERT QUERY */
+INSERT INTO TABLE_NAME( País,CódigoISOpaís,Oficinaderegistro,Grupoedad,Edad(años),ÁreaConocimiento,SubAreaConocimiento,NivelAcadémico,Estadocivil,Género,Etniadelapersona,Estatura(CM),Localización,Cantidaddepersonas )
+VALUES
+(
+    'ALEMANIA','DEU','C. BERLIN','ADULTO',35,'CIENCIAS SOCIALES Y HUMANAS','CIENCIA POLÍTICA Y/O RELACIONES INTERNACIONALES','PREGRADO - PROFESIONAL','SOLTERO','MASCULINO','NINGUNA',-1,'(51.165691, 10.451526)',1
+);
+
+/* INSERT QUERY */
+INSERT INTO TABLE_NAME( País,CódigoISOpaís,Oficinaderegistro,Grupoedad,Edad(años),ÁreaConocimiento,SubAreaConocimiento,NivelAcadémico,Estadocivil,Género,Etniadelapersona,Estatura(CM),Localización,Cantidaddepersonas )
+VALUES
+(
+    'ESPAÑA','ESP','C. BARCELONA','ADULTO',36,'NO INDICA','NO INDICA','POSTGRADO - MAESTRIA','SOLTERO','FEMENINO','OTRO',165,'(40.463667, -3.74922)',1
+);
+
+/* INSERT QUERY */
+INSERT INTO TABLE_NAME( País,CódigoISOpaís,Oficinaderegistro,Grupoedad,Edad(años),ÁreaConocimiento,SubAreaConocimiento,NivelAcadémico,Estadocivil,Género,Etniadelapersona,Estatura(CM),Localización,Cantidaddepersonas )
+VALUES
+(
+    'CANADA','CAN','C. MONTREAL','ADULTO',35,'CIENCIAS SOCIALES Y HUMANAS','GEOGRAFÍA O HISTORIA','NO INDICA','SOLTERO','FEMENINO','OTRO',-1,'(56.130366, -106.346771)',1
+);
+
+/* INSERT QUERY */
+INSERT INTO TABLE_NAME( País,CódigoISOpaís,Oficinaderegistro,Grupoedad,Edad(años),ÁreaConocimiento,SubAreaConocimiento,NivelAcadémico,Estadocivil,Género,Etniadelapersona,Estatura(CM),Localización,Cantidaddepersonas )
+VALUES
+(
+    'VENEZUELA','VEN','C. MARACAIBO','ADULTO MAYOR',91,'CIENCIAS SOCIALES Y HUMANAS','DERECHO Y AFINES','NO INDICA','SOLTERO','FEMENINO','NINGUNA',-1,'(6.42375, -66.58973)',1
+);
+
+/* INSERT QUERY */
+INSERT INTO TABLE_NAME( País,CódigoISOpaís,Oficinaderegistro,Grupoedad,Edad(años),ÁreaConocimiento,SubAreaConocimiento,NivelAcadémico,Estadocivil,Género,Etniadelapersona,Estatura(CM),Localización,Cantidaddepersonas )
+VALUES
+(
+    'REINO UNIDO','GBR','C. LONDRES','ADULTO',51,'NO INDICA','NO INDICA','PREGRADO - TÉCNICO PROFESIONAL','SOLTERO','MASCULINO','NINGUNA',169,'(55.378051, -3.435973)',1
+);
+
+/* INSERT QUERY */
+INSERT INTO TABLE_NAME( País,CódigoISOpaís,Oficinaderegistro,Grupoedad,Edad(años),ÁreaConocimiento,SubAreaConocimiento,NivelAcadémico,Estadocivil,Género,Etniadelapersona,Estatura(CM),Localización,Cantidaddepersonas )
+VALUES
+(
+    'CANADA','CAN','C. CALGARY','ADULTO',35,'INGENIERÍA, ARQUITECTURA Y AFINES','INGENIERÍA EN SISTEMAS, TELEMÁTICA Y AFINES','POSTGRADO - MAESTRIA','DESCONOCIDO','MASCULINO','OTRO',-1,'(56.130366, -106.346771)',1
+);
+
+/* INSERT QUERY */
+INSERT INTO TABLE_NAME( País,CódigoISOpaís,Oficinaderegistro,Grupoedad,Edad(años),ÁreaConocimiento,SubAreaConocimiento,NivelAcadémico,Estadocivil,Género,Etniadelapersona,Estatura(CM),Localización,Cantidaddepersonas )
+VALUES
+(
+    'MEXICO','MEX','C. GUADALAJARA','ADOLESCENTE',12,'NINGUNA','NINGUNA','PRIMARIA','SOLTERO','FEMENINO','OTRO',-1,'(23.634501, -102.552784)',1
+);
+
+/* INSERT QUERY */
+INSERT INTO TABLE_NAME( País,CódigoISOpaís,Oficinaderegistro,Grupoedad,Edad(años),ÁreaConocimiento,SubAreaConocimiento,NivelAcadémico,Estadocivil,Género,Etniadelapersona,Estatura(CM),Localización,Cantidaddepersonas )
+VALUES
+(
+    'PANAMA','PAN','C. PANAMA','ADULTO',58,'NO INDICA','NO INDICA','PREGRADO - PROFESIONAL','CASADO','MASCULINO','NINGUNA',-1,'(8.537981, -80.782127)',1
+);
+
+/* INSERT QUERY */
+INSERT INTO TABLE_NAME( País,CódigoISOpaís,Oficinaderegistro,Grupoedad,Edad(años),ÁreaConocimiento,SubAreaConocimiento,NivelAcadémico,Estadocivil,Género,Etniadelapersona,Estatura(CM),Localización,Cantidaddepersonas )
+VALUES
+(
+    'REPUBLICA DOMINICANA','DOM','C. SANTO DOMINGO','ADULTO',45,'CIENCIAS DE LA SALUD','MEDICINA','NO INDICA','CASADO','FEMENINO','OTRO',-1,'(18.735693, -70.162651)',1
+);
+
+/* INSERT QUERY */
+INSERT INTO TABLE_NAME( País,CódigoISOpaís,Oficinaderegistro,Grupoedad,Edad(años),ÁreaConocimiento,SubAreaConocimiento,NivelAcadémico,Estadocivil,Género,Etniadelapersona,Estatura(CM),Localización,Cantidaddepersonas )
+VALUES
+(
+    'VENEZUELA','VEN','C. PUERTO ORDAZ','ADULTO JOVEN',22,'NO INDICA','NO INDICA','BACHILLERATO','SOLTERO','FEMENINO','SIN ETNIA REGISTRADA',135,'(6.42375, -66.58973)',1
+);
+
+/* INSERT QUERY */
+INSERT INTO TABLE_NAME( País,CódigoISOpaís,Oficinaderegistro,Grupoedad,Edad(años),ÁreaConocimiento,SubAreaConocimiento,NivelAcadémico,Estadocivil,Género,Etniadelapersona,Estatura(CM),Localización,Cantidaddepersonas )
+VALUES
+(
+    'ESTADOS UNIDOS','USA','C. NEWARK','ADULTO',48,'CIENCIAS DE LA SALUD','MEDICINA','POSTGRADO - DOCTORADO','CASADO','FEMENINO','OTRO',-1,'(37.09024, -95.712891)',1
+);
+
+/* INSERT QUERY */
+INSERT INTO TABLE_NAME( País,CódigoISOpaís,Oficinaderegistro,Grupoedad,Edad(años),ÁreaConocimiento,SubAreaConocimiento,NivelAcadémico,Estadocivil,Género,Etniadelapersona,Estatura(CM),Localización,Cantidaddepersonas )
+VALUES
+(
+    'VENEZUELA','VEN','C. VALENCIA VEN','ADULTO',34,'CIENCIAS SOCIALES Y HUMANAS','PERIODISMO, COMUNICACIÓN SOCIAL Y AFINES','NO INDICA','SOLTERO','MASCULINO','NINGUNA',-1,'(6.42375, -66.58973)',1
+);
+
+/* INSERT QUERY */
+INSERT INTO TABLE_NAME( País,CódigoISOpaís,Oficinaderegistro,Grupoedad,Edad(años),ÁreaConocimiento,SubAreaConocimiento,NivelAcadémico,Estadocivil,Género,Etniadelapersona,Estatura(CM),Localización,Cantidaddepersonas )
+VALUES
+(
+    'VENEZUELA','VEN','C. CARACAS','ADULTO MAYOR',87,'NINGUNA','NINGUNA','PRIMARIA','CASADO','FEMENINO','NINGUNA',157,'(6.42375, -66.58973)',1
+);
+
+/* INSERT QUERY */
+INSERT INTO TABLE_NAME( País,CódigoISOpaís,Oficinaderegistro,Grupoedad,Edad(años),ÁreaConocimiento,SubAreaConocimiento,NivelAcadémico,Estadocivil,Género,Etniadelapersona,Estatura(CM),Localización,Cantidaddepersonas )
+VALUES
+(
+    'VENEZUELA','VEN','C. BARQUISIMETO','ADULTO',31,'CIENCIAS SOCIALES Y HUMANAS','SOCIOLOGÍA, TRABAJO SOCIAL Y AFINES','NO INDICA','SOLTERO','FEMENINO','NINGUNA',-1,'(6.42375, -66.58973)',1
+);
+
+/* INSERT QUERY */
+INSERT INTO TABLE_NAME( País,CódigoISOpaís,Oficinaderegistro,Grupoedad,Edad(años),ÁreaConocimiento,SubAreaConocimiento,NivelAcadémico,Estadocivil,Género,Etniadelapersona,Estatura(CM),Localización,Cantidaddepersonas )
+VALUES
+(
+    'ITALIA','ITA','C. MILAN','ADULTO',58,'CIENCIAS SOCIALES Y HUMANAS','PERIODISMO, COMUNICACIÓN SOCIAL Y AFINES','NO INDICA','SOLTERO','FEMENINO','OTRO',-1,'(41.87194, 12.56738)',1
+);
